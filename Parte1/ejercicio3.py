b648915ffcfdaeadd23728c0066421239066c2a5
juanpavilla <juanpavilla29@gmail.com>
2023-06-28 15:08:49 -0500

    cambio en el ejercicio tres

--- Parte1/ejercicio3.py
+++ Parte1/ejercicio3.py
@@ -7,4 +7,4 @@ for digito in binario:
     decimal += int(digito) * (2 ** exp)
     exp -= 1
 
-print("El número decimal equivalente es:", decimal)
+print("El número decimal equivalente a binario es :", decimal)
